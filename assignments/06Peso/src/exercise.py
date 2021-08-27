be95d7616ddffb83677d6642a4c4a3086b6d98c1
jperezrangel <88467921+jperezrangel@users.noreply.github.com>
2021-08-26 20:13:09 -0500

    Ajustes del Quiz de Programacion 1: Programas que hacen cálculos.

--- assignments/06Peso/src/exercise.py
+++ assignments/06Peso/src/exercise.py
@@ -1,12 +1,6 @@
 def main():
     #escribe tu código abajo de esta línea
-    inicial = float(input("Dame el peso inicial: "))
-    final = float(input("Dame el peso final: "))
-    meses = int(input("Dame la cantidad de meses: "))
-    kilos = (inicial-final)/meses
-    print("Lo que debes bajar por mes es:",kilos)
-
-
+    pass
 
 if __name__ == '__main__':
     main()
